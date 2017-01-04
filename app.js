c42362d1ef2f0a245d40730afde93363862c2a91
Naomi Kudren <N.Kudren@amaze.com>
2017-01-04 16:25:57 +0000

    adding nodemailer

--- app.js
+++ app.js
@@ -2,31 +2,85 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var storage = [];
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+var smtpTransport = require('nodemailer-smtp-transport');
+var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
+var mailOptions;
+
+var smtpTransport = nodemailer.createTransport(smtpTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'softwaretextil1234@gmail.com',
+        pass: '******'
+    }
+}));
+app.post('/email', urlencodedParser, function(req, res) {
+
+
+    var mailOptions = {
+        from: '"Website" <naomikudren@gmail.com>', // sender address
+        to: "softwaretextil1234@gmail.com", // list of receivers
+        subject: 'Contact form from website ', // Subject line
+        text: `Name:${req.body.name}, Phone:${req.body.phone}, ${req.body.email}, Message:${req.body.message}`,
+
+    };
+    console.log(req)
+    smtpTransport.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+
+    res.redirect("/");
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+});
+
+
 
 // Sets the static file folders
-app.use("/js", express.static(__dirname + "/js"));
-app.use("/css", express.static(__dirname + "/css"));
+app.use(express.static(__dirname + '/public'));
 
 // Send & response function
 
 // set port
 var port = (process.env.PORT || 9000);
 server.listen(port, function() {
-	console.log('Server is running...');
+    console.log('Server is running...');
 });
 
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-	console.log("Page requested");
+    res.sendFile(__dirname + '/index.html');
+    console.log("Page requested");
 });
+
+
+app.get("/products", function(req, res) {
+    res.sendFile(__dirname + '/products.html');
+
+})
+
+app.get("/index", function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+
+})
+
 
 
 
 // All other paths result in 404
 app.get('*', function(req, res) {
-	res.status(404).end();
-	// .sendFile(__dirname + '/404.html');
+    res.status(404).end();
+    // .sendFile(__dirname + '/404.html');
 });
-
-
